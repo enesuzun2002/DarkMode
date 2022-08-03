e455db977430d99ff16025f37a7402ed3913c8e1
enesuzun2002 <emurat.uzun@gmail.com>
2022-08-03 23:54:59 +0300

    DarkMode: implement functionality

--- app/src/main/java/com/enesmuratuzun/darkmode/FirstFragment.kt
+++ app/src/main/java/com/enesmuratuzun/darkmode/FirstFragment.kt
@@ -1,18 +1,19 @@
 package com.enesmuratuzun.darkmode
 
+import android.app.UiModeManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.appcompat.app.AppCompatActivity
 import com.enesmuratuzun.darkmode.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
+    private var uiModeManager: UiModeManager? = null
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -23,17 +24,41 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        uiModeManager = activity?.getSystemService(AppCompatActivity.UI_MODE_SERVICE) as UiModeManager
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        super.onViewCreated(view, savedInstanceState)
+        binding.toggleButton.addOnButtonCheckedListener { _, checkedId, _ ->
+            when (checkedId) {
+                binding.button1.id -> {
+                    setDarkMode(0)
+                }
+                binding.button2.id -> {
+                    setDarkMode(1)
+                }
+                else -> {
+                    setDarkMode(2)
+                }
+            }
+        }
+    }
+
+    private fun setDarkMode(value: Int) {
+        when (value){
+            2 -> {
+                uiModeManager?.nightMode = UiModeManager.MODE_NIGHT_NO
+            }
+            1 -> {
+                uiModeManager?.nightMode = UiModeManager.MODE_NIGHT_AUTO
+            }
+            else -> {
+                uiModeManager?.nightMode = UiModeManager.MODE_NIGHT_YES
+            }
         }
     }
 
---
+++ app/src/main/java/com/enesmuratuzun/darkmode/Theme.kt
@@ -0,0 +1,12 @@
+package com.enesmuratuzun.darkmode
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class Theme: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
