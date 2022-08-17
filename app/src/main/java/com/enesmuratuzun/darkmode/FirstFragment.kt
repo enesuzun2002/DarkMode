58c2c1cf803be21ffd6706e89a18da0b9b5b607a
Enes Murat Uzun <emurat.uzun@gmail.com>
2022-08-17 12:37:12 +0300

    DarkMode: add google admob ads

--- app/src/main/java/com/enesmuratuzun/darkmode/FirstFragment.kt
+++ app/src/main/java/com/enesmuratuzun/darkmode/FirstFragment.kt
@@ -1,22 +1,24 @@
 package com.enesmuratuzun.darkmode
 
 import android.app.UiModeManager
-import android.content.Context
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.enesmuratuzun.darkmode.databinding.FragmentFirstBinding
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+    private var mInterstitialAd: InterstitialAd? = null
     private var uiModeManager: UiModeManager? = null
     private var _binding: FragmentFirstBinding? = null
 
@@ -35,6 +37,7 @@ class FirstFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        getInterstitial()
 
         super.onViewCreated(view, savedInstanceState)
         var darkMode = activity?.getDrawable(R.drawable.ic_dark_mode)
@@ -43,32 +46,109 @@ class FirstFragment : Fragment() {
         when(uiModeManager?.nightMode){
             UiModeManager.MODE_NIGHT_NO -> {
                 binding.imageView.setImageDrawable(lightMode)
-                binding.toggleButton.check(binding.button2.id)
+                if(!Constants.isStarted){
+                    binding.toggleButton.check(binding.button2.id)
+                    Constants.isStarted = true
+                }
+
             }
             UiModeManager.MODE_NIGHT_YES -> {
                 binding.imageView.setImageDrawable(darkMode)
-                binding.toggleButton.check(binding.button1.id)
+                if(!Constants.isStarted){
+                    binding.toggleButton.check(binding.button1.id)
+                    Constants.isStarted = true
+                }
             }
         }
 
         binding.toggleButton.addOnButtonCheckedListener { _, checkedId, _ ->
-            when (checkedId) {
-                binding.button1.id -> {
+            if(Constants.isStarted){
+                if (checkedId == binding.button1.id && uiModeManager?.nightMode != UiModeManager.MODE_NIGHT_YES) {
                     setDarkMode(1)
                 }
-                else -> {
+                else if(checkedId == binding.button2.id && uiModeManager?.nightMode != UiModeManager.MODE_NIGHT_NO){
                     setDarkMode(0)
                 }
             }
         }
+
+        val mAdView = AdView(requireActivity())
+        mAdView.setAdSize(AdSize.FULL_BANNER)
+        if(BuildConfig.DEBUG){
+            mAdView.adUnitId= "ca-app-pub-3940256099942544/6300978111"
+        } else {
+            mAdView.adUnitId= Constants.bannerAdId
+        }
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+        binding.adViewContainer.addView(mAdView)
+
     }
     private fun setDarkMode(value: Int) {
+        Constants.count++
         when (value){
             0 -> {
                 uiModeManager?.nightMode = UiModeManager.MODE_NIGHT_NO
             }
             else -> {
                 uiModeManager?.nightMode = UiModeManager.MODE_NIGHT_YES
+            }
+        }
+        if(Constants.count == 5){
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.show(requireActivity())
+            } else {
+                Log.d("TAG", "The interstitial ad wasn't ready yet.")
+            }
+            Constants.count = 0
+        }
+    }
+
+    fun getInterstitial() {
+        var adRequest = AdRequest.Builder().build()
+        var adUnitId: String = if(BuildConfig.DEBUG){
+            "ca-app-pub-3940256099942544/1033173712"
+        } else {
+            Constants.interstitialAdId
+        }
+
+        InterstitialAd.load(requireActivity(),adUnitId, adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError?.toString()!!)
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
+        mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+            override fun onAdClicked() {
+                // Called when a click is recorded for an ad.
+                Log.d(TAG, "Ad was clicked.")
+            }
+
+            override fun onAdDismissedFullScreenContent() {
+                // Called when ad is dismissed.
+                Log.d(TAG, "Ad dismissed fullscreen content.")
+                mInterstitialAd = null
+            }
+
+            override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                // Called when ad fails to show.
+                Log.e(TAG, "Ad failed to show fullscreen content.")
+                mInterstitialAd = null
+            }
+
+            override fun onAdImpression() {
+                // Called when an impression is recorded for an ad.
+                Log.d(TAG, "Ad recorded an impression.")
+            }
+
+            override fun onAdShowedFullScreenContent() {
+                // Called when ad is shown.
+                Log.d(TAG, "Ad showed fullscreen content.")
             }
         }
     }
--- app/src/main/java/com/enesmuratuzun/darkmode/MainActivity.kt
+++ app/src/main/java/com/enesmuratuzun/darkmode/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.enesmuratuzun.darkmode
 
-import android.app.UiModeManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.enesmuratuzun.darkmode.databinding.ActivityMainBinding
-import com.google.android.material.color.DynamicColors
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
+        MobileAds.initialize(this) {}
 
     }
 
